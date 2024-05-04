f84ac11f47ffeee00dabfe674e13438d8379716c
Paul Wieland <paul.wieland@student.reutlingen-university.de>
2024-05-03 23:15:06 -0300

    third try

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+'''In dieser Datei wird eine Zeichenfolge test in der Konsole zurückgegeben'''
 print("test")
 print("test test")
-print("test\ttest")
+print("testtest")
 print('test "test       "')
+
+
+
+def tet(a, b):
+    '''diese Funktion fügt zwei buchstaben zusammen und fügt ein kommer zwischen ihnen ein'''
+    return a + ',' + b
+
+print(tet("Hello", "World"))
